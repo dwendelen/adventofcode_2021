3e48b17798e83b199478b0e925e3ee63292be41e
Daan Wendelen <daanwendelen@gmail.com>
2021-12-20 18:02:48 +0100

    Day20

---
+++ src/day20/day20_b.clj
@@ -0,0 +1,76 @@
+(def input
+  (line-seq ( clojure.java.io/reader "input.txt"))
+)
+
+(defn to-bit [char]
+  (if (= \# char) 1 0)
+)
+
+(def table
+  (vec (map to-bit (first input)) )
+)
+
+
+(def initial-image
+  (mapv #(mapv to-bit %) (rest (rest input)))
+)
+
+(defn toIdx [bits]
+  (reduce #(+ (* %1 2) %2) bits)
+)
+
+(defn process2 [e11 e12 e13 r1 e21 e22 e23 r2 e31 e32 e33 r3 acc]
+  (let [ idx (toIdx [e11 e12 e13 e21 e22 e23 e31 e32 e33])
+         val (get table idx)
+         new-acc (conj acc val)
+       ]
+    (if (empty? r1)
+      new-acc
+      (recur e12 e13 (first r1) (rest r1) e22 e23 (first r2) (rest r2) e32 e33 (first r3) (rest r3) new-acc)
+    )
+  )
+)
+
+(defn process1 [r1 r2 r3 rst acc emp]
+  (let [ two-zeros (repeat 2 emp)
+         e13 (first r1)
+         rr1 (concat (rest r1) two-zeros)
+         e23 (first r2)
+         rr2 (concat (rest r2) two-zeros)
+         e33 (first r3)
+         rr3 (concat (rest r3) two-zeros)
+         new-row (process2 emp emp e13 rr1 emp emp e23 rr2 emp emp e33 rr3 [])
+         new-acc (conj acc new-row)
+       ]
+    (if (empty? rst)
+      new-acc
+      (recur r2 r3 (first rst) (rest rst) new-acc emp)
+    )
+  )
+)
+
+; [lines empty]
+(defn process [lines emp]
+  (let [ n (count lines)
+         zeros (repeat n emp)
+         first-line (first lines)
+         rest-line (rest lines)
+         two-zeros (repeat 2 zeros)
+         others (concat rest-line two-zeros)
+         new-line (process1 zeros zeros first-line others [] emp)
+         new-empty (get table (toIdx (repeat 9 emp)))
+       ]
+    [new-line new-empty]
+  )
+)
+
+
+; not 19578 is too high
+(let [ [p1 e1] (reduce (fn [[p e] _] (process p e)) [initial-image 0] (range 50))
+       m1 (map #(reduce + %) p1)
+       r (reduce + m1)
+     ]
+  (println (count p1) (count (first p1)))
+  (println r)
+)
+
